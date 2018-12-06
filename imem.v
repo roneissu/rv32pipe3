3d67da5d8d879e6c794ea6fe1474e452ea697d8b
roneissu <sadeluisronaldo@gmail.com>
2018-12-06 09:48:37 -0200

    Pipeline completo

--- imem.v
+++ imem.v
@@ -16,22 +16,55 @@
 
 endmodule*/
 
-module imem (clk, addr, data);
-	input				clk;
+module imem (addr, data);
 	input		[9:0]	addr;
 	output	reg	[31:0]	data;
 
 	reg [31:0] rom;
 
-	always @(clk or addr)
+	always @(addr)
 	begin
 	  case (addr)
-	    'd0 : data = 'h00000533;
-	    'd1 : data = 'h00250513;
-	    'd2 : data = 'h40a505b3;
-	    'd3 : data = 'h40a58633;
-	    'd4 : data = 'h40a606b3;
-	    'd5 : data = 'h40a68733;
+	    'd0 	: data = 'h00000533;
+	    'd1 	: data = 'h00250513;
+	    'd2 	: data = 'h40a505b3;
+	    'd3 	: data = 'h0035c613;
+	    'd4 	: data = 'h00a5f733;
+	    'd5 	: data = 'h00676693;
+	    'd6 	: data = 'h00b02023;
+	    'd7 	: data = 'h00002783;
+	    'd8 	: data = 'h00000533;
+	    'd9 	: data = 'h000005b3;
+	    'd10 	: data = 'h00b50463;
+	    'd11 	: data = 'h0000006f;
+	    'd12 	: data = 'h00000533;
+	    'd13 	: data = 'h00000533;
+	    'd14 	: data = 'h00250513;
+	    'd15 	: data = 'h40a505b3;
+	    'd16 	: data = 'h0035c613;
+	    'd17 	: data = 'h00a5f733;
+	    'd18 	: data = 'h00676693;
+	    'd19 	: data = 'h00b02023;
+	    'd20 	: data = 'h00002783;
+	    'd21 	: data = 'h00000533;
+	    'd22 	: data = 'h000005b3;
+	    'd23 	: data = 'h00000533;
+	    'd24 	: data = 'h00250513;
+	    'd25 	: data = 'h40a505b3;
+	    'd26 	: data = 'h0035c613;
+	    'd27 	: data = 'h00a5f733;
+	    'd28 	: data = 'h00676693;
+	    'd29 	: data = 'h00b02023;
+	    'd30 	: data = 'h00002783;
+	    'd31 	: data = 'h00000533;
+	    'd32 	: data = 'h000005b3;
+	    'd33 	: data = 'h00000000;
+	    'd34 	: data = 'h00000000;
+	    'd35 	: data = 'h00000000;
+	    'd36 	: data = 'h00000000;
+	    'd37 	: data = 'h00000000;
+	    'd38 	: data = 'h00000000;
+	    'd39 	: data = 'h00000000;
 	    default : data = 'h0;
 	  endcase
 	end
--- rv32pipe3.v
+++ rv32pipe3.v
@@ -1,16 +1,20 @@
 module rv32pipe3 (clk, reset, results);
+
+	// Ports ------------------------------------------------------------------
 	input clk, reset;
 	output [31:0] results;
 	
+	// Datapath wires ---------------------------------------------------------
 	wire [31:0] pc_4, pc_shft, pc_mux, inst, data1, data2, dataW, imm_ext, shft, data2_mux, alu_res, data_mem;
 	wire [0:12] ctrl;
 	
-	// Fetch & Decode
+	// Fetch & Decode ---------------------------------------------------------
 	
 	// Mux_PC
-	assign pc_mux = (ctrl[0]) ? x_mw[100:69] : pc_4;
+	assign pc_mux = (x_mw_ctrl[0]) ? x_mw[100:69] : pc_4;
 	// End Mux_PC
 	
+	// PC Register ############################################################
 	reg [31:0] pc;
 	always @(posedge clk)
 	begin
@@ -23,56 +27,69 @@ module rv32pipe3 (clk, reset, results);
 				pc <= pc_mux;
 			end
 	end
+	// End PC Register ########################################################
 	
-//	imem imem_1 (pc[9:0], inst);
-	imem imem_1 (clk, pc[9:0], inst);
+	// Instruction Memory
+	imem imem_1 (pc[9:0], inst);
+//	imem imem_1 (clk, pc[9:0], inst);
 	
 	// PC+4
 	assign pc_4 = pc + 'd1;
 	// End PC+4
 	
-	registers regs_1 (clk, ctrl[1], inst[19:15], inst[24:20], x_mw[4:0], data1, data2, dataW);
+	// Register File
+	registers regs_1 (clk, x_mw_ctrl[1], inst[19:15], inst[24:20], x_mw[4:0], data1, data2, dataW);
 	
+	// Immediate extender
 	immgen immgen_1 (ctrl[2:4], inst[31:7], imm_ext);
 	
+	// Controller
 	controller controller_1 ({inst[30],inst[14:12],inst[6:0]}, ctrl);
 
-	// Pipeline Register FETCH/DECODE | EXECUTE
+	// Pipeline Register FETCH/DECODE | EXECUTE ###############################
 	reg [132:0] fd_x;
+	reg [0:8]	fd_x_ctrl;
 	always @(posedge clk)
 	begin
 		fd_x <= {pc, data1, data2, imm_ext, inst[11:7]};
+		fd_x_ctrl <= {ctrl[0:1], ctrl[5:11]};
 	end
+	// ########################################################################
 
-	// Execute
+	// Execute ----------------------------------------------------------------
 	
-	//shifter shifter_1 (imm_ext, shft);
 	// Shifter
 	assign shft = (imm_ext << 1);
+	// End Shifter
 
 	// PC+Shift
 	assign pc_shft = fd_x[132:101] + shft;
 	// End PC+Shift
 	
 	// Mux_ALU
-	assign data2_mux = (ctrl[5]) ? fd_x[36:5] : fd_x[68:37];
+	assign data2_mux = (fd_x_ctrl[2]) ? fd_x[36:5] : fd_x[68:37];
 	// End Mux_ALU
 	
-	alu alu_1 (fd_x[100:69], data2_mux, ctrl[6:9], alu_res);
+	// Arithmetic-Logical Unity
+	alu alu_1 (fd_x[100:69], data2_mux, fd_x_ctrl[3:6], alu_res);
 	
-	// Pipeline Register EXECUTE | MEMORY/WRITEBACK
+	// Pipeline Register EXECUTE | MEMORY/WRITEBACK ###########################
 	reg [100:0] x_mw;
+	reg [0:3]	x_mw_ctrl;
 	always @(posedge clk)
 	begin
 		x_mw <= {pc_shft, alu_res, fd_x[68:37], fd_x[4:0]};
+		x_mw_ctrl <= {fd_x_ctrl[0:1], fd_x_ctrl[7:8]};
 	end
+	// ########################################################################
 	
-	// Memory & WriteBack
+	// Memory & WriteBack -----------------------------------------------------
 	
-	dmem dmem_1 (ctrl[10], clk, x_mw[46:37], x_mw[36:5], data_mem);
+	// Data Memory
+	dmem dmem_1 (x_mw_ctrl[2], clk, x_mw[46:37], x_mw[36:5], data_mem);
 
 	// Mux_WB
-	assign dataW = (ctrl[11]) ? data_mem : x_mw[68:37];
+	assign dataW = (x_mw_ctrl[3]) ? data_mem : x_mw[68:37];
 	// End Mux_WB
 	
 	//
